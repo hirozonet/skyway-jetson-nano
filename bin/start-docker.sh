108daa571161fd2f9e30658a56fe43b7993b5abc
Hiroshi Nakatani <nakatani@hirozo.net>
2021-07-21 18:20:31 +0900

    remove docker platform warning

--- bin/start-docker.sh
+++ bin/start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker run -itd --name gateway -p 8000:8000 -p 50000-50063:50000-50063/udp hirozonet/skyway-jetson-nano
+docker run --platform linux/arm -itd --name gateway -p 8000:8000 -p 50000-50063:50000-50063/udp hirozonet/skyway-jetson-nano
 until [ "`docker inspect -f {{.State.Running}} gateway`"=="true" ]; do sleep 0.5; done
 sleep 1.0
